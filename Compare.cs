5d1731312df8cdf91d29e82246606a60f41678bd
Allred1 <113635259+Allred1@users.noreply.github.com>
2022-10-25 19:50:24 -0600

    update

--- Compare.cs
+++ Compare.cs
@@ -15,20 +15,20 @@ class Compare {
 
     // take action depeding on whether there was a letter in the word or not
     // Has 4 parameters: bool answer (after determining "letterInWord"), the letter, the actual random word, and the number of lives. 
-    public string correctOrIncorrect(bool letterInWord, string letter, string word, int updateLives) {
+    public string correctOrIncorrect(bool letterInWord, string letter, string word, int lives) {
         // call the display word method here
         display.displayWord(word, letter);
                 
         if (letterInWord == true) {
-            display.createParachuteGuy(updateLives);
+            display.createParachuteGuy(lives);
             // make the letter appear over its underscore
             return letter;
         } 
         else {
-            updateLives = player.loseLife(updateLives);
-            display.createParachuteGuy(updateLives);
+            lives = player.loseLife(lives);
+            display.createParachuteGuy(lives);
             // call the update the parachute visual
-            return updateLives + "Try again!";
+            return lives + "Try again!";
         } 
         
     }    
